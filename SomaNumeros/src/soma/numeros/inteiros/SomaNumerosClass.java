600fa066630bb8cca27e39a664023c39ef41cd46
Lucas Godoy <Lucas Godoy@DESKTOP-YODOG>
2024-08-21 21:11:44 -0300

    Primeiro commit: adicionando o código básico para soma de números inteiros.

---
+++ SomaNumeros/src/soma/numeros/inteiros/SomaNumerosClass.java
@@ -0,0 +1,12 @@
+package soma.numeros.inteiros;
+
+public class SomaNumerosClass {
+	public static void main(String[] args) {
+		
+		int a = 2;
+        int b = 3;
+        int soma = a + b;
+        
+        System.out.println(soma);     
+	}
+}
